be7b9d06b05ae7d87a9448dc255921bde6c55899
Islam <arais01@adm.huddinge.se>
2021-12-20 13:29:34 +0100

    doing update functionality

--- profile.php
+++ profile.php
@@ -1,4 +1,6 @@
 <?php
+$update = true;
+
 session_start();
 include "_connect.php";
 $row = array();
@@ -14,6 +16,19 @@ $sql = "SELECT `KundId`, `KundNamn`, `Adress`, `Postadress`, `Tel`, `MobilTel`, 
 $result = mysqli_query($conn, $sql);
 if($result){
     $row = mysqli_fetch_assoc($result);
+    $_SESSION['KundId'] = $row['KundId'];
+}
+if($_SERVER['REQUEST_METHOD'] == 'POST' && $update){
+    $new_username = $_SESSION['username'] = $_POST['username'];
+    $new_addres = $_SESSION['addres'] = $_POST['addres'];
+    $new_postaddres = $_SESSION['postaddres'] = $_POST['post-addres'];
+    $new_telefon = $_SESSION['telefon'] = $_POST['telefon'];
+    $new_mobiltelefon = $_SESSION['mobiltelefon'] = $_POST['mobil-telefon'];
+    $new_email = $_SESSION['email'] = $_POST['email'];
+    $new_pass = $_SESSION['pass'] = $_POST['password'];
+
+    $updateSql = "UPDATE `kund` SET `KundNamn`='$new_username',`Adress`='$new_addres',`Postadress`='$new_postaddres',`Tel`='$new_telefon',`MobilTel`='$new_mobiltelefon',`Epost`='$new_email',`Password`='$new_pass' WHERE 'KundId = '.$_SESSION['KundId']";
+    $updateResult = mysqli_query($conn, $sql);
 }
 ?>
 <!DOCTYPE html>
@@ -30,7 +45,10 @@ if($result){
 <body>
     <form method="post" action="profile.php">
         <h1 class="heading"><span>Profile</span></h1>
-        <h3 class="userId">User Id: <span><?php echo $row['KundId'];?></span></h3>
+        <div class="yourId">
+            <h3 class="userId">User Id: <span><?php echo $row['KundId'];?></span></h3>
+            <span class="message">Your user id will be essential to log in. Make sure you remember it.</span>
+        </div>
         <input name="username" type="text" placeholder="username" class="box" value=<?php echo $row['KundNamn'];?>>
         <input name="addres" type="text" placeholder="addres" class="box" value=<?php echo $row['Adress'];?>>
         <input name="post-addres" type="text" placeholder="post-addres" class="box" value=<?php echo $row['Postadress'];?>>

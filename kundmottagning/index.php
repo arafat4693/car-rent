286640f9b4e61cca7e32e5d3a27fb300afb328fd
Islam <arais01@adm.huddinge.se>
2022-02-21 13:35:26 +0100

    making the staff page

--- kundmottagning/index.php
+++ kundmottagning/index.php
@@ -4,14 +4,6 @@
     if($_SESSION['staffLogin'] != true){
         header("location: stafflogin.php");
     }
-
-    // if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    //     $signout = $_POST['signout'];
-    //     if($signout){
-    //         $_SESSION['loggedin'] = false;
-    //         header("location: login.php");
-    //     }
-    // }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -25,13 +17,37 @@
 <body>
     <section class="staff_container">
         <h1 class="heading" style="margin-top:10px;">Rented <span>Cars</span></h1>
-        <div class="rented_cars">
-            <p>your id: <span>a</span></p>
-            <p>your name: <span>b</span></p>
-            <p>regnr: <span>c</p>
-            <p>utdatum: <span>d</p>
-            <p>indatum: <span>e</p>
-            <p>Insurance: <span>f</span></p>
+        <div class="staff_user">
+            <?php 
+            $staffSql = "SELECT * FROM hyr ORDER BY Indatum ASC";
+            $staffSqlQuery = mysqli_query($conn, $staffSql);
+            if($staffSqlQuery){
+                while($row = mysqli_fetch_assoc($staffSqlQuery)){
+                    $reg = $row['Regnr'];
+                    $matarSql = "SELECT Matarstallning FROM bil WHERE Regnr='$reg'";
+                    $matarRes = mysqli_query($conn, $matarSql);
+                    $row2 = mysqli_fetch_assoc($matarRes);
+            ?>
+            <form class="rented_cars">
+                <p>Customer Id: <span><?php echo $row['KundId']?></span></p>
+                <p>Booking Date: <span><?php echo $row['Bokningsdatum']?></span></p>
+                <p>regnr: <span><?php echo $row['Regnr']?></p>
+                <p>Number of km out: <span><?php echo $row2['Matarstallning']?></span></p>
+                <p>Number of km in: <input type="number"/></p>
+                <p>Out date: <span><input type="text" value="<?php echo $row['Utdatum']?>"/></p>
+                <p>Entry date: <input type="text" value="<?php echo $row['Indatum']?>"/></p>
+                <p>Fuel cost: <input type="number"/></p>
+                <select id="options" name="hyrtyp">
+                    <option value="korttid" <?php echo ($row['Hyrtyp'] === 'korttid' || $row['Hyrtyp'] === 'Korttid') ? 'selected' : null?>>short term days</option>
+                    <option value="veckoslut" <?php echo ($row['Hyrtyp'] === 'veckoslut') ? 'selected' : null?>>weekend</option>
+                    <option value="veckoslutfri" <?php echo ($row['Hyrtyp'] === 'veckoslutfri') ? 'selected' : null?>>weekend free</option>
+                </select>
+                <input type="submit" value="count" class="btn">
+            </form>
+            <?php
+                }
+            }
+            ?>
         </div>
     </section>
 </body>
